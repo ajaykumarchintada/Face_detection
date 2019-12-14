d77b7a11280b16172aa4fbf853f8869ac2258232
Ajay Kumar <48199989+ajaykumarchintada@users.noreply.github.com>
2019-12-14 11:38:38 +0530

    Add files via upload
    created a face detection using computer vision with python 
    sources used are har_cascade_files you can download and use them 
    it will detect your using your web camera and detect your face and eyes 
    for the same programme you can try it using video if you dont  have web camera

---
+++ Face_recognition.py
@@ -0,0 +1,32 @@
+import numpy as np 
+import cv2 as cv 
+face_cascade=cv.CascadeClassifier("/Users/alpha/Desktop/face_detection/haar-cascade-files-master/haarcascade_frontalface_alt2.xml")
+eyes_cascade=cv.CascadeClassifier("/Users/alpha/Desktop/face_detection/haar-cascade-files-master/haarcascade_eye_tree_eyeglasses.xml")
+#loading the harcascade using CascadeClassifier methon in cv
+cap=cv.VideoCapture(0)#argument as 0 because we are using the web camera here if you have multiple cameras u can use 1,2,3,..
+while True:
+    ret,frame=cap.read()#ret value will be true 
+    gray=cv.cvtColor(frame,cv.COLOR_BGR2GRAY) #converting every frame into gray cause open cv works with well with bnw
+    face=face_cascade.detectMultiScale(gray,scaleFactor=1.5,minNeighbors=5) #detecting the face scaleFactor will be for accuarcy 
+    #and here wer are using the scalefactor and minneighbors as per the documentation
+    
+    
+    for(x,y,w,h) in face:
+        cv.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2) #finding the co_ordinates of detected face and drawing rectangle aroudn it
+        roi_gray=gray[y:y+h,x:x+w] #region of index for the detected face 
+        roi_image=frame[y:y+h,x:x+w]
+        
+        
+        eyes=eyes_cascade.detectMultiScale(roi_gray) #detect the eyes in face so passed roi_gray 
+        
+        for(ex,ey,ew,eh) in eyes:
+            cv.rectangle(roi_image,(ex,ey),(ex+ew,ey+eh),(255,0,255),2) #draw rectangles around the eyes
+
+            
+    cv.imshow("Face_Detection",frame)
+    k=cv.waitKey(25)&0xFF
+
+    if k==27:
+        break
+cap.release()
+cv.destroyAllWindows()    
